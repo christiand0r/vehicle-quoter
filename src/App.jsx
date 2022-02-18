18438164563ee2a487797e48fb9131f9f7ff679b
Christian Osorio <christiandoor.2001@gmail.com>
2022-02-18 11:17:05 -0400

    Estado inicial de Resumen de selecciones, paso de props a componentes hijos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import { useState } from 'react';
 import styled from '@emotion/styled';
+import Form from './components/Form';
 import Header from './components/Header';
+import Resume from './components/Resume';
+import Result from './components/Result';
 
 const Container = styled.div`
   max-width: 600px;
@@ -11,12 +15,30 @@ const FormContainer = styled.div`
   padding: 3rem;
 `;
 
+const initialResume = {
+  result: 0,
+  data: {
+    brand: '',
+    plan: '',
+    year: '',
+  },
+};
+
 function App() {
+  const [resume, setResume] = useState(initialResume);
+
+  //Extraemos data
+  const { result, data } = resume;
+
   return (
     <Container>
       <Header title='Cotizador de Seguros' />
 
-      <FormContainer></FormContainer>
+      <FormContainer>
+        <Form setResume={setResume}></Form>
+        <Resume data={data} />
+        <Result result={result} />
+      </FormContainer>
     </Container>
   );
 }
